becd4e7694262c30cef64af1ca07ff1c5d98862e
Mohammad Salim <jd2474ev@gmail.com>
2015-08-06 12:19:55 -0400

    Rails app setup with Bootstrap framework

---
+++ app/models/post.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  content    :string
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Post < ActiveRecord::Base
+  belongs_to :user
+end
